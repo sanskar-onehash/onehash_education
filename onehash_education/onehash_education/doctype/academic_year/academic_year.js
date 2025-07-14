6719d43089ba4e3336a37a1a6027f2e9f6b265d0
Sanskar Soni <sanskarsoni300@gmail.com>
2025-07-14 15:52:56 +0530

    feat: added programs
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

--- onehash_education/onehash_education/doctype/academic_year/academic_year.js
+++ onehash_education/onehash_education/doctype/academic_year/academic_year.js
@@ -1,8 +1,30 @@
 // Copyright (c) 2025, OneHash and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Academic Year", {
-// 	refresh(frm) {
+frappe.ui.form.on("Academic Year", {
+  refresh: function (frm) {
+    setupAcademicYearUI(frm);
+  },
+  add_all: async function (frm) {
+    const currentPrograms =
+      frm.doc.programs?.map((program) => program.program) || [];
+    const otherPrograms = await frappe.db.get_list("Program", {
+      fields: ["name"],
+      filters: { name: ["not in", currentPrograms] },
+    });
 
-// 	},
-// });
+    for (let otherProgram of otherPrograms) {
+      frm.add_child("admissions_open_for", { program: otherProgram.name });
+    }
+    frm.refresh_field("admissions_open_for");
+  },
+  clear: function (frm) {
+    frm.set_value("admissions_open_for", []);
+  },
+});
+
+function setupAcademicYearUI(frm) {
+  frm.$wrapper
+    .find("[data-fieldname='add_all'], [data-fieldname='clear']")
+    .css({ display: "inline-block" });
+}
