e66a398545e95ea0b643094ea4e9ce136be0f6de
Sanskar Soni <sanskarsoni300@gmail.com>
2025-07-14 14:50:31 +0530

    feat: initial required setup
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

---
+++ onehash_education/onehash_education/doctype/academic_term/academic_term.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2025, OneHash and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class AcademicTerm(Document):
+
+    def before_insert(self):
+
+        if not self.title:
+            self.update({"title": f"{self.academic_year} ({self.term_name})"})
