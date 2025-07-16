9127e5082971547baa85ea5284f61c7f27a7b114
Sanskar Soni <sanskarsoni300@gmail.com>
2025-07-16 08:42:44 +0530

    fix: first/last name for guardian

--- onehash_education/onehash_education/doctype/guardian/guardian.py
+++ onehash_education/onehash_education/doctype/guardian/guardian.py
@@ -6,4 +6,20 @@ from frappe.model.document import Document
 
 
 class Guardian(Document):
-	pass
+
+    def validate(self):
+        self.set_missing_values()
+
+    def set_missing_values(self):
+
+        # Full name
+        if (
+            not self.get("guardian_name")
+            or self.has_value_changed("first_name")
+            or self.has_value_changed("last_name")
+        ):
+            self.update(
+                {
+                    "guardian_name": f"{self.get('first_name')} {self.get('last_name', default='')}"
+                }
+            )
