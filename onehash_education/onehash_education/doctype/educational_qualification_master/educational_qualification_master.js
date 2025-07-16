e662aefc8bc2a248316de758b392b78c818c1e23
Sanskar Soni <sanskarsoni300@gmail.com>
2025-07-16 14:55:48 +0530

    feat: add student applicant and reqd doctypes
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

---
+++ onehash_education/onehash_education/doctype/educational_qualification_master/educational_qualification_master.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, OneHash and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Educational Qualification Master", {
+// 	refresh(frm) {
+
+// 	},
+// });
