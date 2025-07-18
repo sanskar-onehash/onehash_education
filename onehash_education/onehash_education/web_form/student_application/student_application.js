08dfab7f84f4250390cd41d82a15ce83c43d4011
Sanskar Soni <sanskarsoni300@gmail.com>
2025-07-18 13:04:51 +0530

    feat(applicant): add submitted & custom actions & styles fix
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

--- onehash_education/onehash_education/web_form/student_application/student_application.js
+++ onehash_education/onehash_education/web_form/student_application/student_application.js
@@ -1,3 +1,50 @@
-frappe.ready(function() {
-	// bind events here
-})
+frappe.ready(function () {
+  const frm = frappe.web_form;
+  const $submitBtn = $(".submit-btn");
+
+  setupForm();
+
+  function setupForm() {
+    setupFormUI();
+  }
+
+  function setupFormUI() {
+    // `FIXES:` clientside app web.html template
+    // Wrap page-content in container
+    $(".page-content-wrapper").append($("<div class='container my-4'><div>"));
+    $(".page-content-wrapper .container.my-4").append(
+      $(".page-content-wrapper .page_content"),
+    );
+
+    addCustomActionButtons();
+  }
+
+  function addCustomActionButtons() {
+    // Custom Save button
+    const $customSaveBtn = $(
+      `<div class='btn submit-btn-custom btn-info'>${frm.button_label}</div>`,
+    );
+    $customSaveBtn.click(handleCustomSave);
+
+    // Custom Save & Submit Button
+    const $customSaveAndSubmitBtn = $(
+      `<div class='btn submit-btn-custom btn-warning'>Save & Submit</div>`,
+    );
+    $customSaveAndSubmitBtn.click(handleCustomSaveAndSubmit);
+
+    $(".web-form-head .web-form-actions").append([
+      $customSaveBtn,
+      $customSaveAndSubmitBtn,
+    ]);
+  }
+
+  function handleCustomSave(e) {
+    $submitBtn.click();
+  }
+
+  function handleCustomSaveAndSubmit(e) {
+    frm.set_value("submitted", 1);
+    $submitBtn.click();
+  }
+});
+
