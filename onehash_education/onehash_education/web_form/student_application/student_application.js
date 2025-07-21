f8dd9ecb3147611f2a0fb2284244d5de22c86651
Sanskar Soni <sanskarsoni300@gmail.com>
2025-07-21 09:22:17 +0530

    feat(Applicant): allow webform saving without reqd fields
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

--- onehash_education/onehash_education/web_form/student_application/student_application.js
+++ onehash_education/onehash_education/web_form/student_application/student_application.js
@@ -11,26 +11,61 @@ frappe.ready(function () {
   function setupFormUI() {
     // `FIXES:` clientside app web.html template
     // Wrap page-content in container
-    $(".page-content-wrapper").append(
-      $("<div class='container my-4 fix-container'><div>"),
-    );
-    $(".page-content-wrapper .fix-container").append(
-      $(".page-content-wrapper .page_content"),
-    );
+    if (!$(".page-content-wrapper .container .page-content").length) {
+      $(".page-content-wrapper").append(
+        $("<div class='container my-4 fix-container'><div>"),
+      );
+      $(".page-content-wrapper .fix-container").append(
+        $(".page-content-wrapper .page_content"),
+      );
+    }
 
-    addCustomActionButtons();
+    setupDependsOn();
+    setupActionButtons();
   }
 
-  function addCustomActionButtons() {
+  async function setupDependsOn() {
+    if (!frm.doc.naming_series) {
+      // As initial fields are dependant on naming_series
+      await frm.set_value(
+        "naming_series",
+        frm.fields_dict.naming_series.df.default,
+      );
+    }
+    frm.fields_list.forEach((field) => {
+      if (
+        field.df.mandatory_depends_on &&
+        frm.evaluate_depends_on_value(field.df.mandatory_depends_on)
+      ) {
+        frm.set_df_property(field.df.fieldname, "reqd", 1);
+      }
+
+      if (
+        field.df.read_only_depends_on &&
+        frm.evaluate_depends_on_value(field.df.read_only_depends_on)
+      ) {
+        frm.set_df_property(field.df.fieldname, "read_only", 1);
+      }
+
+      if (
+        field.df.depends_on &&
+        frm.evaluate_depends_on_value(field.df.depends_on)
+      ) {
+        frm.set_value(field.df.fieldname, "hidden", 1);
+      }
+    });
+  }
+
+  function setupActionButtons() {
     // Custom Save button
     const $customSaveBtn = $(
-      `<div class='btn submit-btn-custom btn-info'>${frm.button_label}</div>`,
+      `<div class='btn save-btn-custom btn-info'>${frm.button_label}</div>`,
     );
     $customSaveBtn.click(handleCustomSave);
 
     // Custom Save & Submit Button
     const $customSaveAndSubmitBtn = $(
-      `<div class='btn submit-btn-custom btn-warning'>Save & Submit</div>`,
+      `<div class='btn submit-btn-custom btn-primary'>Save & Submit</div>`,
     );
     $customSaveAndSubmitBtn.click(handleCustomSaveAndSubmit);
 
@@ -41,6 +76,12 @@ frappe.ready(function () {
   }
 
   function handleCustomSave(e) {
+    // Unmark mandatory fields
+    frm.fields_list.forEach((field) => {
+      if (field.df.reqd) {
+        frm.set_df_property(field.df.fieldname, "reqd", 0);
+      }
+    });
     $submitBtn.click();
   }
 
@@ -49,4 +90,3 @@ frappe.ready(function () {
     $submitBtn.click();
   }
 });
-
