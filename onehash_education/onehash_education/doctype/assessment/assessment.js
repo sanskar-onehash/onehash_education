7a6f3815e7794c5c701632301957c7f1f552742c
Sanskar Soni <sanskarsoni300@gmail.com>
2025-08-06 09:00:57 +0530

    feat(assessment): add send assessment button
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

--- onehash_education/onehash_education/doctype/assessment/assessment.js
+++ onehash_education/onehash_education/doctype/assessment/assessment.js
@@ -1,8 +1,46 @@
 // Copyright (c) 2025, OneHash and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Assessment", {
-// 	refresh(frm) {
+frappe.ui.form.on("Assessment", {
+  refresh(frm) {
+    addAssessmentActions(frm);
+  },
+});
 
-// 	},
-// });
+function addAssessmentActions(frm) {
+  if (!frm.is_new()) {
+    addSendInternalAssessmentBtn(frm);
+  }
+}
+
+function addSendInternalAssessmentBtn(frm) {
+  if (!frm.doc.is_internal) return;
+
+  const BTN_LABEL = "Send Assessment";
+  frm.page.remove_inner_button(BTN_LABEL);
+  frm.page.add_inner_button(
+    BTN_LABEL,
+    () => {
+      frappe.show_alert("Sending Assessment Link...");
+      frappe.call({
+        method:
+          "onehash_education.onehash_education.doctype.assessment.assessment.send_assessment_link",
+        args: {
+          assessment: frm.doc.name,
+        },
+        callback: function (res) {
+          if (res?.message === "success") {
+            frappe.show_alert({
+              indicator: "green",
+              message: "Assessment link sent successfully.",
+            });
+          } else {
+            frappe.throw(`Error sending assessment: ${res}`);
+          }
+        },
+      });
+    },
+    null,
+    "primary",
+  );
+}
--- onehash_education/onehash_education/doctype/assessment/assessment.py
+++ onehash_education/onehash_education/doctype/assessment/assessment.py
@@ -1,9 +1,64 @@
 # Copyright (c) 2025, OneHash and contributors
 # For license information, please see license.txt
 
-# import frappe
+
+import frappe
+from frappe.email.doctype.email_template.email_template import get_email_template
 from frappe.model.document import Document
+from frappe.core.doctype.communication.email import make
 
 
 class Assessment(Document):
-	pass
+    pass
+
+
+@frappe.whitelist()
+def send_assessment_link(assessment):
+    assessment_doc = frappe.get_doc("Assessment", assessment)
+
+    if not assessment_doc.is_internal:
+        frappe.throw("Assessment is not internal.")
+    if not assessment_doc.assessment_master:
+        frappe.throw("Assessment Master is not set.")
+
+    assessment_master_doc = frappe.get_doc(
+        "Assessment Master", assessment_doc.assessment_master
+    )
+
+    assessment_link = f"{frappe.utils.get_url()}/assessment?assessment={frappe.utils.quote(assessment)}"
+    applicant_name = frappe.db.get_value(
+        "Student Applicant", assessment_doc.applicant, "applicant_name"
+    )
+
+    subject = "Action Required: Complete Your Assessment"
+    message = """Dear {0},
+
+I hope this message finds you well.
+
+Please complete the following assessment using the link below:
+
+{1}""".format(
+        applicant_name, assessment_link
+    )
+    if assessment_master_doc.email_template:
+        email_template = get_email_template(
+            assessment_master_doc.email_template,
+            {
+                "assessment_link": assessment_link,
+                "applicant_name": applicant_name,
+                **assessment_doc.as_dict(),
+            },
+        )
+        subject = email_template.get("subject")
+        message = email_template.get("message")
+
+    make(
+        doctype=assessment_doc.doctype,
+        name=assessment_doc.name,
+        content=message,
+        subject=subject,
+        recipients=frappe.db.get_value("User", assessment_doc.assessment_for, "email"),
+        send_email=True,
+    )
+
+    return "success"
