d87333c9cabf080e67755fd9f7e95c95b08c3037
Sanskar Soni <sanskarsoni300@gmail.com>
2025-08-10 14:10:37 +0530

    feat(api): add endpoint to get open admissions data
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

--- onehash_education/api.py
+++ onehash_education/api.py
@@ -17,3 +17,19 @@ def get_admission_year_groups(academic_year):
             "parent": academic_year,
         },
     )
+
+
+@frappe.whitelist(allow_guest=True)
+def get_admission_years_and_groups():
+    admission_data = {}
+
+    admission_years = get_admission_years()
+
+    for admission_year in admission_years:
+        admission_year_group = get_admission_year_groups(admission_year["name"])
+
+        admission_data[admission_year["name"]] = [
+            yg["year_group"] for yg in admission_year_group
+        ]
+
+    return admission_data
