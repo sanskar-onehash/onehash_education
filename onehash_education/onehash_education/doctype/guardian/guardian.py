4413d88128f55bde1ce03094542afb7bde427090
Sanskar Soni <sanskarsoni300@gmail.com>
2025-09-07 09:39:41 +0530

    feat(guardian): set full name on save if required
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

--- onehash_education/onehash_education/doctype/guardian/guardian.py
+++ onehash_education/onehash_education/doctype/guardian/guardian.py
@@ -10,16 +10,20 @@ class Guardian(Document):
     def before_insert(self):
         self.set_missing_values()
 
-    def set_missing_values(self):
+    def validate(self):
+        self.set_missing_values()
 
+    def set_missing_values(self):
         # Full name
         if (
             not self.get("guardian_name")
             or self.has_value_changed("first_name")
             or self.has_value_changed("last_name")
         ):
-            self.update(
-                {
-                    "guardian_name": f"{self.get('first_name')}{(self.get('last_name') or '') and f' {self.last_name}'}"
-                }
+            self.set_full_name()
+
+    def set_full_name(self):
+        if self.first_name:
+            self.guardian_name = " ".join(
+                filter(None, [self.first_name, self.last_name])
             )
