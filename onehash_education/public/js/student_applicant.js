18d0f2fd6e555eb749f1497609aa1f804b50316b
Sanskar Soni <sanskarsoni300@gmail.com>
2025-09-16 09:36:25 +0530

    fix(applicant): revert list view custom logic to standard approach
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

--- onehash_education/public/js/student_applicant.js
+++ onehash_education/public/js/student_applicant.js
@@ -15,16 +15,8 @@ if (window.location.pathname === "/student-application/list") {
           `/student-application/${frappe.web_form_list.data[0].name}`;
       }
     }, 300);
-  });
 
-  frappe.call({
-    method:
-      "onehash_education.onehash_education.doctype.student_applicant.student_applicant.get_applicant_custom_scripts",
-    callback: (res) => {
-      if (res && res.message) {
-        runCustomScript(res.message.script, res.message.style);
-      }
-    },
+    runCustomScript();
   });
 } else if (window.location.pathname === "/student-application/new") {
   redirectToStudentApplicationList();
@@ -47,13 +39,13 @@ frappe.ready(() => {
   studentApplicationTasks.forEach((task) => task());
 });
 
-function runCustomScript(custom_script, custom_style) {
-  if (custom_script) {
-    const customScript = new Function(custom_script);
+function runCustomScript() {
+  if (frappe.web_form_doc.custom_script) {
+    const customScript = new Function(frappe.web_form_doc.custom_script);
     customScript();
   }
-  if (custom_style) {
-    const $style = $(`<style>${custom_style}</style>`);
+  if (frappe.web_form_doc.custom_style) {
+    const $style = $(`<style>${frappe.web_form_doc.custom_style}</style>`);
     $("head").append($style);
   }
 }
