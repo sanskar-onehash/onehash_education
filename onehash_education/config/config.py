5e698f9653d60cad767908eba66138e5750e0bcd
Sanskar Soni <sanskarsoni300@gmail.com>
2025-10-06 09:07:41 +0530

    feat(portal): set home page for student roles to student-portal
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

--- onehash_education/config/config.py
+++ onehash_education/config/config.py
@@ -2,10 +2,12 @@ ROLES = [
     {
         "name": "Student Applicant",
         "desk_access": 0,
+        "home_page": "/student-portal",
     },
     {
         "name": "Student",
         "desk_access": 0,
+        "home_page": "/student-portal",
     },
     {"name": "Admissions User"},
     {"name": "Admissions Manager"},
