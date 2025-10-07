a41c83215dcbac70f49cd652052f39b079c0ff4e
Sanskar Soni <sanskarsoni300@gmail.com>
2025-10-07 16:42:36 +0530

    fix(student): swap applicant role to student's on student insert
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

--- onehash_education/onehash_education/doctype/student/student.py
+++ onehash_education/onehash_education/doctype/student/student.py
@@ -1,9 +1,15 @@
 # Copyright (c) 2025, OneHash and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Student(Document):
-	pass
+
+    def before_insert(self):
+
+        if self.user_id:
+            user = frappe.get_doc("User", self.user_id)
+            user.remove_roles("Student Applicant")
+            user.add_roles("Student")
