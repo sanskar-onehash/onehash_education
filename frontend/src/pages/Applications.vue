9cf3ea28a3dfc2180b765257d7f5a4c67c29678d
Sanskar Soni <sanskarsoni300@gmail.com>
2025-10-09 16:56:09 +0530

    fix(portal): don't set application iframe size dynamically on message
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

--- frontend/src/pages/Applications.vue
+++ frontend/src/pages/Applications.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref } from 'vue'
 
 const PAGE_NAME = 'applications'
 
@@ -18,17 +18,4 @@ const iframeSrc = ref('')
 const iframeEl = ref(null)
 
 iframeSrc.value = window.location.origin + '/student-application'
-
-onMounted(() => {
-  window.addEventListener('message', (message) => {
-    if (iframeEl.value && message.source === iframeEl.value.contentWindow) {
-      if (
-        message.data &&
-        ['number', 'string'].includes(typeof message.data.height)
-      ) {
-        iframeEl.value.style.height = `${message.data.height}px`
-      }
-    }
-  })
-})
 </script>
