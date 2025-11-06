3fcaa80e5d6a3d48ca339564f16792a647ba9f63
Sanskar Soni <sanskarsoni300@gmail.com>
2025-11-06 14:27:14 +0530

    feat(withdrawal): prevent duplicate withdrawals for same student
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

--- onehash_education/onehash_education/doctype/student_withdrawal/student_withdrawal.py
+++ onehash_education/onehash_education/doctype/student_withdrawal/student_withdrawal.py
@@ -7,6 +7,25 @@ from frappe.model.document import Document
 
 class StudentWithdrawal(Document):
 
+    def before_insert(self):
+        existing_withdrawal = frappe.db.exists(
+            "Student Withdrawal",
+            {
+                "student": self.student,
+                "docstatus": ["!=", 2],
+                "name": ["!=", self.name],
+            },
+        )
+
+        if existing_withdrawal:
+            frappe.throw(
+                """<a href='{url}'><strong>Student Withdrawal</strong></a> already exists for the student, please cancel it to create new.""".format(
+                    url=frappe.utils.get_url_to_form(
+                        "Student Withdrawal", existing_withdrawal
+                    )
+                )
+            )
+
     def before_submit(self):
         if frappe.db.exists(
             "Enrollment",
